3e127838a008a5c62c2280528b5048f6fef26b31
Berantzino <lass5643@stud.kea.dk>
2019-09-26 08:59:49 +0200

    [ADD] ElevatorDoorScene Animation + Environment Sounds for Tv2Lorry Scenes

---
+++ Assets/Scripts/EventBroadcaster.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class EventBroadcaster : MonoBehaviour
+{
+    public static EventBroadcaster Instance;
+    public VideoPlayer videoPlayer;
+
+    // Acts as a delegate (used to broadcast the occurence of an event)
+    public UnityAction trigger;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Debug.LogWarning("Be aware!!! Another Instance already exists!");
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        if (videoPlayer != null)
+        {
+            Debug.Log("videoPlayer.frame: " + (ulong)videoPlayer.frame);
+            Debug.Log("videoPlayer.frameCount: " + videoPlayer.frameCount);
+            if ((ulong)videoPlayer.frame == videoPlayer.frameCount - 1)
+            {
+                Debug.Log("Video Ended");
+                trigger?.Invoke();
+                videoPlayer.enabled = false;
+                Debug.Log("Same Frame");
+            }
+
+        }
+
+    }
+
+    
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.tag == "Player")
+    //    {
+    //        Debug.Log("OnTriggerEnter Called!");
+    //        trigger?.Invoke();
+    //    }
+    //}
+}
---
+++ Assets/Scripts/EventListener.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventListener : MonoBehaviour
+{
+    //[SerializeField] private EventBroadcaster broadcaster;
+    [SerializeField] private UnityEvent OnTriggered;
+    private bool isSubbed = false;
+
+    private void Start()
+    {
+        if (!isSubbed)
+        {
+            EventBroadcaster.Instance.trigger += OnCallTrigger;
+            isSubbed = true;
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (EventBroadcaster.Instance != null && !isSubbed)
+        {
+            isSubbed = true;
+            EventBroadcaster.Instance.trigger += OnCallTrigger;
+        }
+    }
+
+    private void OnDisable()
+    {
+        EventBroadcaster.Instance.trigger -= OnCallTrigger;
+        isSubbed = false;
+    }
+
+    private void OnCallTrigger()
+    {
+        Debug.Log("OnCallTrigger Called!");
+        OnTriggered?.Invoke();
+    }
+}
