b8e4a551f629a23b3917d630408ccb2b624e34a3
Berantzino <lass5643@stud.kea.dk>
2019-09-26 13:55:00 +0200

    [ADD] Studio Scene - Construction Info. Status = 2/4

---
+++ Assets/Scripts/UIPopUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPopUp : MonoBehaviour
+{
+    public bool autoHide = false;
+
+    private Text popupText;
+    //private Animator anim;
+
+    private float hideDelay = 3f;
+
+    void Start()
+    {
+        popupText = GetComponentInChildren<Text>();
+        //anim = GetComponent<Animator>();
+    }
+
+    public void ShowPopUp(string msg)
+    {
+        popupText.text = msg;
+        //anim.SetTrigger("ShowPopUp");
+        if (autoHide)
+            StartCoroutine(DelayHide());
+    }
+
+    private IEnumerator DelayHide()
+    {
+        yield return new WaitForSeconds(hideDelay);
+        //anim.SetTrigger("HidePopUp");
+    }
+}
